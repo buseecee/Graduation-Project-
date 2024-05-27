33a5f946e3edbc7a312c5032f1c49fad135b7fb2
Buse <busecee0@gmail.com>
2024-05-27 20:46:24 +0300

    sayfaların ui'ları tamamlandı

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./templates/*.html", "./static/src/**/*.css"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
---
+++ website/views.py
@@ -0,0 +1,25 @@
+from flask import Blueprint,render_template
+
+views = Blueprint('views', __name__)
+
+@views.route('/')
+def index():
+    return render_template('base.html')
+
+@views.route('/login')
+def login():
+    return render_template('login.html')
+
+@views.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@views.route('/authorized_login')
+def authorized_login():
+    return render_template('authorized_login.html')
+
+@views.route('/teacher_login')
+def teacher_login():
+    return render_template('teacher_login.html')
+
+# Diğer görünüm fonksiyonları buraya eklenebilir
