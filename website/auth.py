3cb3b99e68db697a5c19d5e2ead12de52af57133
Buse <busecee0@gmail.com>
2024-05-27 23:34:34 +0300

    01-signup ilk hali

--- website/auth.py
+++ website/auth.py
@@ -1,4 +1,6 @@
-from flask import Blueprint
+import sqlite3
+from flask import Blueprint, request
+import os
 
 auth = Blueprint('auth', __name__)
 
@@ -6,11 +8,44 @@ auth = Blueprint('auth', __name__)
 def login():
     return '<p>Login Page</p>'
 
-@auth.route('/register')
-def register():
-    return 'Register Page'
+def get_db_connection():
+    conn = sqlite3.connect('C:/Users/Buse Ece/Desktop/sqlite.db')
+    conn.row_factory = sqlite3.Row
+    return conn
 
+@auth.route('/signup', methods=['POST'])
+def signup():
+    name = request.form['name']
+    surname = request.form['surname']
+    email = request.form['email']
+    password = request.form['password']
+    confirm_password=request.form['confirm_password']
 
+    conn = get_db_connection()
+    
+      # SELECT sorgusu için yeni bir cursor oluşturuyoruz
+    select_cursor = conn.cursor()
+    select_cursor.execute("SELECT * FROM teacher WHERE NAME=?", (name,))
 
-# Diğer kimlik doğrulama işlevleri buraya eklenebilir
-
+    # SELECT sorgusu sonucunu alıyoruz
+    existing_record = select_cursor.fetchone()
+ # Eğer kayıt varsa hata ver ve işlemi sonlandır
+    if existing_record:
+        select_cursor.close()
+        conn.close()
+        return 'User already exists!'
+    
+    # SELECT sorgusu için kullandığımız cursor'ı kapatıyoruz
+    select_cursor.close()
+    
+    # INSERT işlemi için yeni bir cursor oluşturuyoruz
+    insert_cursor = conn.cursor()
+    
+    # INSERT sorgusunu çalıştırıyoruz
+    insert_cursor.execute("INSERT INTO teacher (name, surname, email, password, confirm_password) VALUES (?, ?, ?, ?, ?)", (name, surname, email, password, confirm_password))
+    
+    # İşlemi tamamla ve bağlantıyı kapat
+    conn.commit()
+    conn.close()
+    
+    return 'Sign up successful!'
